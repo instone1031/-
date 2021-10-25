82d416373ec36b850dc9137b2d3be1f9373a70ad
instone1031 <83266404+instone1031@users.noreply.github.com>
2021-10-25 12:03:54 +0900

    Update gwaje.py

--- gwaje.py
+++ gwaje.py
@@ -1,12 +1,12 @@
 score = int(input("당신의 점수는 어느정도입니까?: "))
 
-if score >= 90;
+if score >= 90:
   print("A")
-elif score >= 80;
+elif score >= 80:
   print("B")
-elif score >= 70;
+elif score >= 70:
   print("C")
-else;
+else:
   print("D")
   
-print("당신의 학점은" + score + "입니다.")
+print("당신의 학점은" + str(score) + "입니다.")
